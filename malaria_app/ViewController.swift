3fd482bb88fd90d7a2d26c3945a196d89cc8ff52
vincent <vincent14319@gmail.com>
2019-03-25 17:26:47 -0600

    Initial Commit

---
+++ malaria_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  malaria_app
+//
+//  Created by vicente on 3/25/19.
+//  Copyright © 2019 vicente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
